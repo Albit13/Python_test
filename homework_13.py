e72802bd2884eb023f25a88c4365632e4ea0f5e3
Sergey Albit <Sergey.albit@avanquest.com>
2023-04-10 16:02:52 +0300

    Merge remote-tracking branch 'origin/Home_work' into Home_work

---
+++ homework_13.py
@@ -0,0 +1,14 @@
+def is_valid_email(email):
+    if email.count('@') != 1 or email.count('.') != 1:
+        return False
+    if email.index('@') > email.index('.'):
+        return False
+    if email.startswith('@') or email.endswith('.'):
+        return False
+    return True
+
+email = input("Enter your e-mail: ")
+if is_valid_email(email):
+    print("Email is valid")
+else:
+    print("Email is not valid")
