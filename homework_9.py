6e935173d2f704fb92c27ffe4e7c742ee62a3f55
Sergey Albit <Sergey.albit@avanquest.com>
2023-04-16 01:15:50 +0300

    Merge remote-tracking branch 'origin/Home_work' into Home_work

--- homework_9.py
+++ homework_9.py
@@ -1,13 +1,12 @@
 n = int(input("Enter n: "))
 
 for i in range(1, n + 1):
-
     print(" " * (2 * (n - i)), end="")
-
     for j in range(1, i + 1):
         print(j, end=" ")
     for j in range(i - 1, 0, -1):
         print(j, end=" ")
-
     if i != n:
+        print()
+    else:
         print()
